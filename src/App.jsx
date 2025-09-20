2efb5b8a1652b0b816fe9447da6cfe25e229a2e3
shamimahommad <shamimahommad@gmail.com>
2025-09-20 21:58:07 +0600

    1st time add

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import Countries from './Component/Countries/Countries'
+
+function App() {
+
+  return (
+    <>
+
+      <Countries></Countries>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>IN The countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
